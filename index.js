b1dd211bbfa494727893bb23b700ba22ae1794c2
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-01-15 12:08:54 +0300

    Got to understand some small bits of node with this one

---
+++ index.js
@@ -0,0 +1,27 @@
+import http from 'node:http'
+import fs from 'node:fs'
+
+const paths = [
+  'index.html', 'about.html', 'contact-me.html', '404.html'
+]
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    req.url = '/index'
+  } else if (paths.includes(req.url.slice('.')[0])) {
+    req.url = req.url + '.html';
+  }
+
+  fs.readFile('./src' + req.url + '.html', {encoding: 'utf8'}, (err, data) => {
+    if (err) {
+      return err;
+    }
+
+    res.write(data);
+    res.end();
+  })
+})
+
+server.listen(8080, () => {
+  console.log("--- Server listening on port 8080 ---")
+});
